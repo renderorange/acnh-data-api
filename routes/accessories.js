2faee0956177018cfa3d4222743d12f20a13498c
Blaine Motsinger <blaine@renderorange.com>
2020-07-07 09:04:09 -0500

    Add accessories route

---
+++ routes/accessories.js
@@ -0,0 +1,26 @@
+// trackability-api
+// accessories routes
+
+const express = require( 'express' );
+const router = express.Router();
+const dbh = require( './../lib/dbi' );
+const response = require( './../lib/response' );
+
+const table = 'Accessories';
+
+router.get( '/', function( req, res ) {
+    dbh.get( table )
+        .then( function( rows ) {
+            res.status( response.status.HTTP_OK.code )
+               .header( 'Content-Type', 'application/json' )
+               .send( JSON.stringify( rows ) );
+        })
+        .catch( function( err ) {
+            console.log( err );
+            res.status( response.status.HTTP_INTERNAL_SERVER_ERROR.code )
+               .header( 'Content-Type', 'text/plain' )
+               .send( response.status.HTTP_INTERNAL_SERVER_ERROR.string );
+        });
+});
+
+module.exports = router;
